8b2579690475d58c3c0f6f66b06465acd1a71fc1
chartExpert <vksek49@gmail.com>
2022-04-23 08:41:24 +0900

    add selected image auto slide show and automatic spacing image slide on custom page

---
+++ EatspressionAndroid/app/src/main/java/com/capstone/eatspression/CustomResultActivity.java
@@ -0,0 +1,14 @@
+package com.capstone.eatspression;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_result);
+    }
+}
