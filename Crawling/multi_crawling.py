578d431571da395dc955249caed5f6c7f147ce1a
7xxogre <61622859+7xxogre@users.noreply.github.com>
2022-04-24 05:28:38 +0900

    fix 500 error (internet server error)

--- Crawling/multi_crawling.py
+++ Crawling/multi_crawling.py
@@ -16,30 +16,27 @@ def multi_parser(site):
     chromeOptions.add_argument("window-size=1920x1080")
     chromeOptions.add_argument("disable-gpu")
     chromeOptions.add_argument("lang=ko_KR") # 한국어!
-
     driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=chromeOptions)
-
     return parse(driver, site)
 
 
 def parse(driver, site):
-
     driver.get(site)
+    
     try:
         pic = driver.find_element_by_class_name('_img')
         pic = pic.get_attribute('src')
         pic=pic.replace('type=f180_180&','type=w750&')
         return
     except:
-        print(site)
-        while(BeautifulSoup(driver.page_source,"html.parser").text[:3] == "429"):
+        while(driver.page_source[38] != "네"):
             driver.get(site)
             time.sleep(0.1)
     pic = driver.find_element_by_class_name('_img')
     pic = pic.get_attribute('src')
     pic=pic.replace('type=f180_180&','type=w750&')
-    print(pic)
 
+    
 if __name__ == '__main__': # ， ，pathos
     start=time.time()
     URL="https://s.search.naver.com/p/around/search.naver?tab=restaurant&lat=37.451891&lng=126.6543237&rid=11177540"
@@ -52,13 +49,13 @@ if __name__ == '__main__': # ， ，pathos
     chromeOptions.add_argument("lang=ko_KR") # 한국어!
 
     driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=chromeOptions)
+    
     driver.get(url=URL)
     links = driver.find_elements_by_class_name('bx_area')
     links=links[:15]
     list=[]
     for link in links:
         link=link.get_attribute('href')
-        print(link)
     # https://m.store.naver.com/places/detail?id=36271040
         link=link.replace('store','place')
         link=link.replace('places','restaurant')
