8d29642bed14a8131ff7b5310b580452c78ee590
JJONSOO <pjs970616@naver.com>
2022-05-09 15:07:39 +0000

    Add parameter , Fix return format

--- Backend/model/Restaurant.py
+++ Backend/model/Restaurant.py
@@ -1,22 +1,18 @@
 import pymysql
 
-def get_food_list(x,y):
+def get_food_list(x,y,distance,num):
     food_list = []
-    db = pymysql.connect(host='204.236.163.157',user='JOONSOO',db='test_db',password='2411',charset='utf8')
+    db = pymysql.connect(host='54.183.227.153',user='JOONSOO',db='test_db',password='2411',charset='utf8')
     curs=db.cursor()
-    sql = "SELECT *, ST_DISTANCE_SPHERE(POINT("+str(x)+","+str(y)+"), location) AS dist FROM Restaurant ORDER BY dist Limit 2;"
+    sql = "SELECT *, ST_DISTANCE_SPHERE(POINT("+str(x)+","+str(y)+"), location)"+"AS dist FROM Restaurant WHERE ST_DISTANCE_SPHERE(POINT("+str(x)+","+str(y)+"),location)<"+str(distance)+" ORDER BY dist limit "+str(num)+";"
     print(sql)
     curs.execute(sql)
 
     rows = curs.fetchall()
     for e in rows:
-        temp = {'id':e[0],'img_source':e[2],'link':e[3]}
-        food_list.append(temp)
+        img_source = {'img_source':e[2]}
+        food_list.append(img_source)
     
     db.commit()
     db.close()
     return food_list
-# SET @lon = 126.955869;
-# SET @lat = 37.546037;
- 
-# SELECT *, ST_DISTANCE_SPHERE(POINT(126.955869, 37.546037), location) AS dist FROM place ORDER BY dist Limit 2;
