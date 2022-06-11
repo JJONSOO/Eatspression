d25b64afaf2dfe3fab255f0d90cf1940c5a74593
JJONSOO <75569293+JJONSOO@users.noreply.github.com>
2022-06-11 16:27:51 +0900

    Update Restaurant.py

--- Backend/model/Restaurant.py
+++ Backend/model/Restaurant.py
@@ -1,10 +1,10 @@
 import pymysql
 import random
 def get_food_list(x,y,distance,num):
-    db = pymysql.connect(host='54.241.56.66',user='JOONSOO',db='test_db',password='2411',charset='utf8')
+    db = pymysql.connect(host='13.52.242.111',user='JOONSOO',db='test_db',password='2411',charset='utf8')
     curs=db.cursor()
     # sql = "SELECT *, ST_DISTANCE_SPHERE(POINT("+str(x)+","+str(y)+"), location)"+"AS dist FROM Restaurant WHERE ST_DISTANCE_SPHERE(POINT("+str(x)+","+str(y)+"),location)<"+str(distance)+" ORDER BY dist limit "+str(num)+";"
-    sql = "SELECT picture FROM Restaurant WHERE ST_DISTANCE_SPHERE(POINT("+str(x)+","+str(y)+"),location)<"+str(distance)+";"
+    sql = "SELECT picture , link,name FROM Restaurant_v3 WHERE ST_DISTANCE_SPHERE(POINT("+str(x)+","+str(y)+"),location)<"+str(distance)+";"
     print(sql)
     curs.execute(sql)
 
@@ -14,7 +14,14 @@ def get_food_list(x,y,distance,num):
         # img_source = {'img_source':e[2]}
         # food_list.append(img_source)
         temp=e[0]
-        food_list.append(temp)
+        temp=temp.split(',')
+        
+        random_num=random.randint(0,len(temp)-1)
+        food =[]
+        food.append(temp[random_num][1:-1])
+        food.append(e[1])
+        food.append(e[2])
+        food_list.append(food)
     if(len(food_list)<num):
         food_list=random.sample(food_list,len(food_list))
     else:
@@ -24,7 +31,7 @@ def get_food_list(x,y,distance,num):
     return food_list
 
 def get_food_link(food_picture):
-    db = pymysql.connect(host='54.241.56.66',user='JOONSOO',db='test_db',password='2411',charset='utf8')
+    db = pymysql.connect(host='13.52.242.111',user='JOONSOO',db='test_db',password='2411',charset='utf8')
     curs=db.cursor()
     sql = "SELECT link FROM Restaurant WHERE picture =\""+str(food_picture)+"\";"
     curs.execute(sql)
